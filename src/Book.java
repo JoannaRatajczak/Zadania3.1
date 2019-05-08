7c19b6ece3759ac9eb8fb9ba6970f934eedb22d3
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-05-08 15:24:35 +0200

    1.2 Konstruktor inicjujący pola

--- src/Book.java
+++ src/Book.java
@@ -6,4 +6,15 @@ class Book {
     double price;
     int noOfPages;
     double peoplesRate;
+
+    Book(String tit, String auth, boolean series, double pri, String pub, int pages, double rate){
+        title=tit;
+        author=auth;
+        isSeries=series;
+        publisher=pub;
+        price=pri;
+        noOfPages=pages;
+        peoplesRate=rate;
+
+    }
 }
